853ab881643c7218f16b16ec375bd698bad8d5a7
Erick Nava <ericknava@byui.edu>
2024-03-25 15:20:32 -0600

    Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req,res) => {
-    res.send("Welcome to my Erick's backend application");
+    res.send("Welcome to my Erick's backend application on cloud");
 });
 
 app.get("/erick",(req,res) => {
